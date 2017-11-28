6fda1c9fb1b8269c0fdebf8185058c0b4642fcc2
Jura <juratitov@gmail.com>
2017-11-29 01:45:33 +0200

    Add crypt-js

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,35 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    age: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 100
-    },
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        },
+    }]
 });
 
-module.exports = {User};
+module.exports = {
+    User
+};
